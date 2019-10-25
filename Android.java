c457a58c5163ca3bdd9b50996d594cad274371bc
shoaib332433 <shoaib.shah660@gmail.com>
2019-10-25 10:51:25 +0500

    Add method for print name

--- Android.java
+++ Android.java
@@ -1,5 +1,9 @@
-public void Main(String[] arr){
+public class Main(String[] arr){
     public int a=10;
     public int b=20;
-     
+
+
+    public void printname(String message){
+        System.out.println(message);
+        }
 }
